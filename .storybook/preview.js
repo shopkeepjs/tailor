3a283cf049fb1d88f71989f94184654e1f40eb6c
Chancellor <me@chancellor.tech>
2022-07-28 02:54:07 -0500

    added storybook toolbars for theme and mode switching (#3)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,15 +2,51 @@ import { ShopkeepThemeProvider } from '../src/lib';
 import Background from './Background.svelte';
 
 export const decorators = [
-	(_, { parameters }) => {
-		const { mode } = parameters;
-		return { Component: Background, props: { mode } };
+	(_, context) => {
+		const { mode } = context.parameters;
+		return { Component: Background, props: { mode: mode || context.globals.mode } };
 	},
-	(_, { parameters }) => {
-		const { theme } = parameters;
-		return { Component: ShopkeepThemeProvider, props: { theme } };
+	(_, context) => {
+		const { theme } = context.parameters;
+		return { Component: ShopkeepThemeProvider, props: { theme: theme || context.globals.theme } };
 	}
 ];
+export const globalTypes = {
+	mode: {
+		name: 'Mode',
+		description: 'Global dark or light mode for backgrounds',
+		defaultValue: 'light',
+		toolbar: {
+			// The icon for the toolbar item
+			icon: 'circlehollow',
+			// Array of options
+			items: [
+				{ value: 'light', icon: 'circlehollow', title: 'light' },
+				{ value: 'dark', icon: 'circle', title: 'dark' }
+			],
+			// Property that specifies if the name of the item will be displayed
+			showName: true,
+			dynamicTitle: true
+		}
+	},
+	theme: {
+		name: 'Theme',
+		description: 'Global theme for components',
+		defaultValue: 'purple',
+		toolbar: {
+			// The icon for the toolbar item
+			icon: 'contrast',
+			// Array of options
+			items: [
+				{ value: 'purple', left: '🟣', title: 'purple' },
+				{ value: 'green', left: '🟢', title: 'green' }
+			],
+			// Property that specifies if the name of the item will be displayed
+			showName: true,
+			dynamicTitle: true
+		}
+	}
+};
 
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
