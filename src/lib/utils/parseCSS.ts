c2e2c0e606e27aaae199fb87177f12daf9be44ca
Chancellor <me@chancellor.tech>
2022-08-28 01:24:12 -0500

    add type guard for sizing
    * added husky
    
    * refactored lib
    
    * test
    
    * test
    
    * test
    
    * added script
    
    * feat: added more git flow tooling
    
    Added commitzen and continued tweaking the husky pre-commit tools
    
    BREAKING CHANGE: Need to npm ci
    
    * test
    
    * chore(release): 0.4.0
    
    * chore(release): 0.4.1
    
    * chore(release): 0.4.2
    
    * chore(release): 0.4.3
    
    * stashing changes
    
    * added git push
    
    * added hook messages
    
    * more work on the pre-commit hook
    
    * more work on the pre-commit hook
    
    * more work on the pre-commit hook
    
    * Working storybook, fixed build tools, typescript and test library (#2)
    
    * more work on the pre-commit hook
    
    * working storybook
    
    * added storybook toolbars for theme and mode switching (#3)
    
    * Issue 4 fix GitHub action test failing (#5)
    
    * test
    
    * test
    
    * changed the tsconfig
    
    * changed tsconfig
    
    * adding test runner
    
    * 6 add button capabilities (#10)
    
    * added buttons and beginning of custom styles in js
    
    * finalizing MVP for button
    
    * fixed pre-commit hook
    
    * added tests
    
    * only showed success on successfuly upsert
    
    * adding types to test
    
    * 🪲 fixed type misplacement
    
    * chore(release): 0.4.4
    
    * chore(release): 0.4.5
    
    * chore(release): 0.4.6
    
    * add public tag to npm publish (#14)
    
    * chore(release): 0.4.7
    
    * stuff... (#18)
    
    * stuff...
    
    * removed extended tsconfig
    
    * removed extended tsconfig
    
    * chore(release): 0.5.0
    
    * chore(release): 0.5.1
    
    * merge from staging
    
    * chore(release): 0.6.0
    
    * chore(release): 0.6.1
    
    * finalized polarity fix
    
    * Fixed merge conflict
    
    * 22 add box shadow predefined inputs (#23)
    
    * Development (#21)
    
    * added husky
    
    * refactored lib
    
    * test
    
    * test
    
    * test
    
    * added script
    
    * feat: added more git flow tooling
    
    Added commitzen and continued tweaking the husky pre-commit tools
    
    BREAKING CHANGE: Need to npm ci
    
    * test
    
    * chore(release): 0.4.0
    
    * chore(release): 0.4.1
    
    * chore(release): 0.4.2
    
    * chore(release): 0.4.3
    
    * stashing changes
    
    * added git push
    
    * added hook messages
    
    * more work on the pre-commit hook
    
    * more work on the pre-commit hook
    
    * more work on the pre-commit hook
    
    * Working storybook, fixed build tools, typescript and test library (#2)
    
    * more work on the pre-commit hook
    
    * working storybook
    
    * added storybook toolbars for theme and mode switching (#3)
    
    * Issue 4 fix GitHub action test failing (#5)
    
    * test
    
    * test
    
    * changed the tsconfig
    
    * changed tsconfig
    
    * adding test runner
    
    * 6 add button capabilities (#10)
    
    * added buttons and beginning of custom styles in js
    
    * finalizing MVP for button
    
    * fixed pre-commit hook
    
    * added tests
    
    * only showed success on successfuly upsert
    
    * adding types to test
    
    * 🪲 fixed type misplacement
    
    * chore(release): 0.4.4
    
    * chore(release): 0.4.5
    
    * chore(release): 0.4.6
    
    * add public tag to npm publish (#14)
    
    * chore(release): 0.4.7
    
    * stuff... (#18)
    
    * stuff...
    
    * removed extended tsconfig
    
    * removed extended tsconfig
    
    * chore(release): 0.5.0
    
    * chore(release): 0.5.1
    
    * merge from staging
    
    * chore(release): 0.6.0
    
    * chore(release): 0.6.1
    
    * finalized polarity fix
    
    * Fixed merge conflict
    
    * added box shadow support for css in js
    
    * chore(release): 0.7.0
    
    * chore(release): 0.7.1
    
    * Added typeguards and autocomplete for sizings

--- src/lib/utils/parseCSS.ts
+++ src/lib/utils/parseCSS.ts
@@ -1,4 +1,5 @@
-import type { Styles } from '../../types';
+import type { Styles, Volume } from '../../types';
+import { volumes } from '../../types';
 
 interface Map {
 	[key: string]: string;
@@ -17,13 +18,18 @@ const boxShadowLookup = {
 	high: '13px 13px 13px -5px rgba(0, 0, 0, 0.5);'
 };
 
+const typeGuard = <T extends string>(value: string, arrayToCompare: readonly string[]): value is T => {
+	if (typeof value !== 'string') return false;
+	return arrayToCompare.includes(value);
+};
+
 type BoxShadow = 'low' | 'medium' | 'high';
 
 const isPredefinedBoxShadow = (key: string): key is BoxShadow => ['low', 'medium', 'high'].includes(key);
 
 export const parse = (styles: Styles) => {
-	return Object.entries(styles).reduce((str: string, [key, value]: [string, string | number]) => {
-		if (key === 'boxShadow' && typeof value === 'string' && isPredefinedBoxShadow(value))
+	return Object.entries(styles).reduce((str: string, [key, value]: [string, unknown]) => {
+		if (key === 'boxShadow' && typeof value === 'string' && typeGuard<Volume>(value, volumes))
 			return (str += `box-shadow: ${boxShadowLookup[value]};`);
 		return (str += `${camelToCSS[key] ? camelToCSS[key] : key}: ${value};`);
 	}, '');
